27aad35c8cce049c6f9579187a0b50151ae64c93
KalasioFR <kalasio.games1@gmail.com>
2024-04-17 22:50:52 +0300

    Added buildings, character model and animations

--- CourseWork/Assets/Scripts/PlayerMotor.cs
+++ CourseWork/Assets/Scripts/PlayerMotor.cs
@@ -4,20 +4,37 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    public Animator animator;
     private bool isGrounded;
     private bool sprinting = false;
-    public float speed = 5f;
+    public float speed = 2f;
     public float gravity = -9.81f;
     public float jumpHeight = 1f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isRunning", false);
+        animator.SetBool("isJumping", false);
     }
 
     void Update()
     {
         isGrounded = controller.isGrounded;
+
+        if(sprinting)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") == 1);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", Input.GetAxisRaw("Vertical") == 1);
+        }
+        animator.SetBool("isJumping", !controller.isGrounded);
     }
 
     public void ProcessMove(Vector2 input)
@@ -36,15 +53,14 @@ public class PlayerMotor : MonoBehaviour
 
     public void Sprint()
     {
-    
         sprinting = !sprinting;
         if (sprinting)
         {
-            speed = 12;
+            speed = 5;
         }
         else
         {
-            speed = 5;
+            speed = 2;
         }
     }
 
